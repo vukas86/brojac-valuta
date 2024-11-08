b388257d01d15ee2283df03f81ca613f07805d3b
“vukas86” <vukasbit@gmail.com>
2024-11-08 03:49:13 +0100

    styling the input

--- src/components/DenominationInput.jsx
+++ src/components/DenominationInput.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { denominations } from "./data";
 import parseDenominationValue from "../utils";
 
+import styles from "./DenominationInput.module.css";
+
 function DenominationInput({
   currency,
   type,
@@ -40,32 +42,34 @@ function DenominationInput({
   };
 
   return (
-    <div>
-      {selectedDenominations.map((denomination) => {
-        const displayValue = denomination
-          .replace("note-", "")
-          .replace("coin-", "");
-        const uniqueKey = `${currency}-${denomination}`;
+    <>
+      <div className={styles.moneyContainer}>
+        {selectedDenominations.map((denomination) => {
+          const displayValue = denomination
+            .replace("note-", "")
+            .replace("coin-", "");
+          const uniqueKey = `${currency}-${denomination}`;
 
-        return (
-          <div key={uniqueKey}>
-            <label>{displayValue}:</label>
-            <input
-              type="number"
-              value={quantities[denomination] || 0}
-              min="0"
-              onChange={(e) =>
-                onQuantityChange(denomination, parseInt(e.target.value) || 0)
-              }
-            />
-          </div>
-        );
-      })}
+          return (
+            <div key={uniqueKey} className={styles.moneyInput}>
+              <label>{displayValue}:</label>
+              <input
+                type="number"
+                value={quantities[denomination] || 0}
+                min="0"
+                onChange={(e) =>
+                  onQuantityChange(denomination, parseInt(e.target.value) || 0)
+                }
+              />
+            </div>
+          );
+        })}
+      </div>
       <button onClick={calculateHandler}>Izbroji</button>
       <button onClick={handleReset}>Resetuj</button>
 
       <div>Total: {total}</div>
-    </div>
+    </>
   );
 }
 
